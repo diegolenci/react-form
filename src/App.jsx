36266c2a4b83f725e6b372364052d0bcb862d8cd
diegolenci <lencidiego0@gmail.com>
2025-02-25 17:01:20 +0100

    npm vite, install e run dev + costanti

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+function App() {
+
+  const [posts, setPosts] = useState([])
+  const [newPost, setNewPost] = useState('')
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
